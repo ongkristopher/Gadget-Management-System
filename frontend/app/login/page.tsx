8438e1fbc52e77c7b4dfef10d4f6baf42d585e74
Kristopher Ong <ongkristopher@gmail.com>
2025-08-21 02:44:14 +0800

    Login page design

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,3 +1,38 @@
+import {
+  Button,
+  Container,
+  Paper,
+  PasswordInput,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import classes from "./Login.module.css";
+
 export default function LoginPage() {
-  return <div>Login page</div>;
+  return (
+    <Container size={420} my={40}>
+      <Title ta="center" className={classes.title}>
+        Welcome back!
+      </Title>
+
+      <Paper withBorder shadow="sm" p={22} mt={30} radius="md">
+        <TextInput
+          label="Username"
+          placeholder="yourusername"
+          required
+          radius="md"
+        />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+          radius="md"
+        />
+        <Button fullWidth mt="xl" radius="md">
+          Log in
+        </Button>
+      </Paper>
+    </Container>
+  );
 }
